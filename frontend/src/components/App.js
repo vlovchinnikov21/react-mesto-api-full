5a3702b728692c37aabf295a83b08d495d7811ae
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-10-14 18:37:18 +0300

    fix: лайки

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -97,7 +97,6 @@ function App() {
     const isLiked = card.likes.some(id => id === currentUser._id);
     
     api.changeLikeCardStatus(card._id, !isLiked).then((newCard) => {
-      console.log(newCard);
       setCards((state) =>
         state.map((c) => (c._id === card._id ? newCard.data : c))
       )
