b865389b09b677fe6b8443871de02c75ea47db4a
Vladimir Ovchinnikov <metalass21@yandex.ru>
2022-10-14 17:36:24 +0300

    fix: запрос

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -39,7 +39,7 @@ function App() {
       api
         .getUserProfile()
         .then((items) => {
-          setCurrentUser(items);
+          setCurrentUser(items.data);
         })
         .catch((err) => console.log(err));
     }
@@ -50,7 +50,7 @@ function App() {
       api
         .getInitialCards()
         .then((items) => {
-          setCards(items);
+          setCards(items.data);
         })
         .catch((err) => console.log(err));
     }
